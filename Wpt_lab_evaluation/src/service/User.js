200f9bda548d2b5b179ec96a2cf475ed3ff1fe0d
Rajni Jambhulkar <147649617+RajniJambhulkar@users.noreply.github.com>
2024-02-20 12:34:20 +0530

    Delete Wpt_lab_evaluation directory

--- Wpt_lab_evaluation/src/service/User.js
+++
@@ -1,8 +0,0 @@
-export class User{
-    constructor(FirstName,lastName,address,pincode){
-        this.FirstName=FirstName;
-        this.lastName=lastName;
-        this.address=address;
-        this.pincode=pincode;
-    }
-}
